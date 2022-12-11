b43e9b9a0d5353e8cf135d48116cb6f6a93a4126
Jules Parham <parhamjules@pop-os.localdomain>
2022-12-11 16:47:20 -0600

    form validation and feedback for login and register forms

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -5,15 +5,29 @@ const { authenticated, unauthenticated } = require('../middlewares');
 const { renderTemplate } = require('../helpers');
 const app = module.exports = express();
 
-app.get('/login', unauthenticated, (req, res) => renderTemplate(res, 'auth/login'));
+app.get('/login', unauthenticated, (req, res) => {
+  const errors = app.locals.errors;
+  app.locals.errors = {};
+  renderTemplate(res, 'auth/login', { errors });
+});
 
 app.post('/login', unauthenticated, (req, res) => {
-  User.findByUsername(req.body.username).then(async user => {
-    if(user && await bcrypt.compare(req.body.password, user.password)) {
-      req.session.currentUser = user;
-    }
-    res.redirect('/');
-  });
+  let errors = {};
+  if(!req.body.username) errors.username = 'Username is required.';
+  if(!req.body.password) errors.password = 'Password is required.';
+  app.locals.errors = errors;
+  if(errors.username || errors.password) {
+    res.redirect('/login');
+  } else {
+    User.findByUsername(req.body.username).then(async user => {
+      if(user && await bcrypt.compare(req.body.password, user.password)) {
+        req.session.currentUser = user;
+      } else {
+        app.locals.errors.password = 'Your login credentials are incorrect.';
+      }
+      res.redirect('/');
+    });
+  }
 });
 
 app.get('/logout', authenticated, (req, res) => {
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -5,19 +5,32 @@ const { unauthenticated } = require('../middlewares');
 const { renderTemplate } = require('../helpers');
 const app = module.exports = express();
 
-app.get('/register', unauthenticated, (req, res) => renderTemplate(res, 'auth/register'));
+app.get('/register', unauthenticated, (req, res) => {
+  const errors = app.locals.errors;
+  app.locals.errors = {};
+  renderTemplate(res, 'auth/register', { errors });
+});
 
 app.post('/register', unauthenticated, (req, res) => {
-  User.findByUsername(req.body.username).then(async found => {
-    if(!found) {
-      const passwordHash = await bcrypt.hash(req.body.password, 8);
-      const user = new User({username: req.body.username, password: passwordHash});
-      user.save().then(newUser => {
-        req.session.currentUser = newUser;
-        res.redirect('/');
-      });
-    } else {
-      res.redirect('/register');
-    }
-  });
+  let errors = {};
+  if(!req.body.username) errors.username = 'Username is required.';
+  if(!req.body.password) errors.password = 'Password is required.';
+  app.locals.errors = errors;
+  if(errors.username || errors.password) {
+    res.redirect('/register');
+  } else {
+    User.findByUsername(req.body.username).then(async found => {
+      if(!found) {
+        const passwordHash = await bcrypt.hash(req.body.password, 8);
+        const user = new User({username: req.body.username, password: passwordHash});
+        user.save().then(newUser => {
+          req.session.currentUser = newUser;
+          res.redirect('/');
+        });
+      } else {
+        app.locals.errors.username = 'That username is already taken. Please choose another.';
+        res.redirect('/register');
+      }
+    });
+  }
 });
